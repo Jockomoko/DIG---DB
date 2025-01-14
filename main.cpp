4401777e740c340a83382cd9cb4e4f87370ba82c
Fabian Åman <Fabianaman03@gmail.com>
2025-01-14 14:16:01 +0100

    added data file and reader

--- main.cpp
+++ main.cpp
@@ -3,4 +3,10 @@
 
 int main()
 {
+    std::fstream filein;
+    std::string line = "";
+    filein.open("DataShopyfiveDataSFData.csv");
+    // int i, Leveransdatum, Fakturadatum, Fakturanr, Typ, Kundnr, Företagstyp, Namn, Adress, Besöksadress, Postnr, Ort, Land, Orgno, Telefon, Fax, Epost, AvvikandeNamn, LeveransAdress, LeveransAdress1, LeveransPostnr, LeveransOrt, AvvikandeBesöksadress, Artikelnr, Benämning, Antal, Enhet, ÁPris, Rabatt, Rabattyp, Moms, PrisInklVat, Total, Inpris;
+    std::getline(filein, line);
+    std::cout << line << std::endl;
 }
