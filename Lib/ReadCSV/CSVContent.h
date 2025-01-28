ef00532ca14c327764282a815e6e00fe68cf237c
Fabian Åman <Fabianaman03@gmail.com>
2025-01-28 18:19:10 +0100

    added database

---
+++ Lib/ReadCSV/CSVContent.h
@@ -0,0 +1,17 @@
+#ifndef CSVContent_H
+#define CSVContent_H
+
+#endif // MACRO
+
+#include <iostream>
+#include <fstream>
+
+class CSVContent
+{
+private:
+    std::string Leveransdatum, Fakturadatum, Fakturanr, Typ, Kundnr, Företagstyp, Namn, Adress, Besöksadress, Postnr, Ort, Land, Orgno, Telefon, Fax, Epost, Avvikande_namn, Leveransadress, Leveransadress_2, Leverans_postnr, Leverans_ort, Avvikande_besöksadress, Artikelnr, Benämning, Antal, Enhet, Á_pris, Rabatt, Rabattyp, Moms, Pris_inkl_vat, Total, Inpris;
+
+public:
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,9 +4,22 @@
 int main()
 {
     std::fstream filein;
-    std::string line = "";
+    std::string line = "", Leveransdatum = "", Fakturadatum = "";
+    int location = 0;
     filein.open("DataShopyfiveDataSFData.csv");
     // int i, Leveransdatum, Fakturadatum, Fakturanr, Typ, Kundnr, Företagstyp, Namn, Adress, Besöksadress, Postnr, Ort, Land, Orgno, Telefon, Fax, Epost, AvvikandeNamn, LeveransAdress, LeveransAdress1, LeveransPostnr, LeveransOrt, AvvikandeBesöksadress, Artikelnr, Benämning, Antal, Enhet, ÁPris, Rabatt, Rabattyp, Moms, PrisInklVat, Total, Inpris;
-    std::getline(filein, line);
-    std::cout << line << std::endl;
+
+    while (getline(filein, line))
+    {
+        std::getline(filein, line);
+        location = line.find(",");
+        Leveransdatum = line.substr(0, location);
+        line = line.substr(location + 1, line.length());
+        location = line.find(",");
+        Fakturadatum = line.substr(0, location);
+        line = line.substr(location + 1, line.length());
+        std::cout << "Fakturadatum: " << Leveransdatum << std::endl;
+        std::cout << "Line: " << line << std::endl;
+        std::cout << "Last: " << Fakturadatum << std::endl;
+    }
 }
